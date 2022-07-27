9d3c9c7c431a78148234c7e74d8800a1afcac28f
hyongti <hyongtiii@gmail.com>
2022-07-27 21:03:03 +0900

    OrbitControls
    스크롤, 스와이프 등을 통해 줌 인/아웃 또는 3D 모델 회전

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -13,6 +14,8 @@ camera.position.z = 2;
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
